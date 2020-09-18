6c574221a7a4a943c5866e705e03831827a6e89f
Lauramars <lauramars85@gmail.com>
2020-09-18 12:04:08 +0200

    Load and Display high scores from local storage

--- end.js
+++ end.js
@@ -25,5 +25,6 @@ saveHighScore = (e) => {
   highScores.splice(5);
 
   localStorage.setItem('highScores', JSON.stringify(highScores));
+  // came back to HomePage
   window.location.assign('/');
 };
