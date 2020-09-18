a5c5e14e777bd610559be5e4ebd3a2ee2a064c73
Lauramars <lauramars85@gmail.com>
2020-09-18 14:18:52 +0200

    Spinning Loader created

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -38,6 +40,7 @@ fetch(
       });
       return formattedQuestion;
     });
+
     startGame();
   })
   .catch((err) => {
@@ -53,6 +56,8 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  game.classList.remove('hidden');
+  loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
